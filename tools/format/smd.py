0e5503dda6a010822f773a97d349333d36dd2a24
Syn Spawacza <synspawacza@gmail.com>
2020-05-14 18:43:13 +0200

    implemented SMD and TMD

---
+++ tools/format/smd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from format.utils import *
+
+
+class Entry:
+    @staticmethod
+    def parse(reader):
+        result = Entry()
+        result.id = read_utf16(reader, 0x80)
+        result.number = read_int(reader, 8)
+        result.text = read_utf16(reader, 0x800)
+        return result
+
+    def serialize(self):
+        result = bytearray()
+        result += write_utf16(self.id, 0x80)
+        result += write_int(self.number, 8)
+        result += write_utf16(self.text, 0x800)
+        return result
+
+
+class File:
+    @staticmethod
+    def parse(reader):
+        result = File()
+        reader.seek(0)
+        count = read_int(reader, 4)
+
+        result.entries = []
+        for i in range(count):
+            result.entries.append(Entry.parse(reader))
+
+        return result
+
+    def serialize(self):
+        result = bytearray()
+        result += write_int(len(self.entries), 4)
+        for e in self.entries:
+            result += e.serialize()
+        return result
---
+++ tools/format/tmd.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from format.utils import *
+
+
+class Entry:
+    @staticmethod
+    def parse(reader):
+        result = Entry()
+        idsize = read_int(reader, 4) * 2
+        result.id = read_utf16(reader, idsize)
+        textsize = read_int(reader, 4) * 2
+        result.text = read_utf16(reader, textsize)
+        return result
+
+    def serialize(self):
+        result = bytearray()
+        idsize = len(self.id) + 1
+        result += write_int(idsize, 4)
+        result += write_utf16(self.id, idsize * 2)
+        textsize = len(self.text) + 1
+        result += write_int(textsize, 4)
+        result += write_utf16(self.text, textsize * 2)
+        return result
+
+
+class File:
+    @staticmethod
+    def parse(reader):
+        result = File()
+        reader.seek(0)
+        count = read_int(reader, 4)
+
+        result.entries = []
+        for i in range(count):
+            result.entries.append(Entry.parse(reader))
+
+        return result
+
+    def serialize(self):
+        result = bytearray()
+        result += write_int(len(self.entries), 4)
+        for e in self.entries:
+            result += e.serialize()
+        return result
