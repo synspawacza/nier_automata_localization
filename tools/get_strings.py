20a611793d7a0f875e3298a0a582d3e490d11fed
Syn Spawacza <synspawacza@gmail.com>
2020-05-16 13:07:54 +0200

    extracted extract_strings_to_file as separate function in get_string.py

--- tools/get_strings.py
+++ tools/get_strings.py
@@ -9,25 +9,57 @@ import format.bin as bin
 import format.mcd as mcd
 import format.properties as properties
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input", help="source file name")
-parser.add_argument("output", help="target file name (*.properties)")
 
-args = parser.parse_args()
-file_ext = os.path.splitext(args.input)[1]
+def extract_strings_to_file(input_filename, output_filename, lang="en"):
+    # TODO: support extracting different languages from .bin files
+    if lang not in {"jp", "en", "fr", "it", "de", "es"}:
+        raise Exception("Language " + lang + " is not supported")
 
-parsed = None
-with open(args.input, "rb") as in_file:
-    if file_ext == ".bin":
-        parsed = bin.File.parse(in_file)
-    elif file_ext == ".smd":
-        parsed = smd.File.parse(in_file)
-    elif file_ext == ".tmd":
-        parsed = tmd.File.parse(in_file)
-    elif file_ext == ".mcd":
-        parsed = mcd.File.parse(in_file)
-    else:
-        raise Exception("Unable to extract " + args.input + ": unknown file extenstion")
+    file_ext = os.path.splitext(input_filename)[1]
 
-with open(args.output, "wb") as out_file:
-    out_file.write(properties.serialize_properties(parsed.get_strings()))
+    parsed = None
+    with open(input_filename, "rb") as in_file:
+        if file_ext == ".bin":
+            parsed = bin.File.parse(in_file)
+        elif file_ext == ".smd":
+            parsed = smd.File.parse(in_file)
+        elif file_ext == ".tmd":
+            parsed = tmd.File.parse(in_file)
+        elif file_ext == ".mcd":
+            parsed = mcd.File.parse(in_file)
+        else:
+            raise Exception(
+                "Unable to extract " + input_filename + ": unknown file extenstion"
+            )
+
+    with open(output_filename, "wb") as out_file:
+        out_file.write(properties.serialize_properties(parsed.get_strings()))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="source file name")
+    parser.add_argument("output", help="target file name (*.properties)")
+    parser.add_argument("--lang", help="source laguage", default="en")
+
+    args = parser.parse_args()
+    extract_strings_to_file(args.input, args.output, args.lang)
+    file_ext = os.path.splitext(args.input)[1]
+
+    parsed = None
+    with open(args.input, "rb") as in_file:
+        if file_ext == ".bin":
+            parsed = bin.File.parse(in_file)
+        elif file_ext == ".smd":
+            parsed = smd.File.parse(in_file)
+        elif file_ext == ".tmd":
+            parsed = tmd.File.parse(in_file)
+        elif file_ext == ".mcd":
+            parsed = mcd.File.parse(in_file)
+        else:
+            raise Exception(
+                "Unable to extract " + args.input + ": unknown file extenstion"
+            )
+
+    with open(args.output, "wb") as out_file:
+        out_file.write(properties.serialize_properties(parsed.get_strings()))
