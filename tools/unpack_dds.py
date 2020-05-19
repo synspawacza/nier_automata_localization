e788036d29ec382e6d74784be175e54755b1e462
Syn Spawacza <synspawacza@gmail.com>
2020-05-19 16:30:02 +0200

    moved unpack.py to tools/unpack_dat.py, minor fixes

--- tools/unpack_dds.py
+++ tools/unpack_dds.py
@@ -4,20 +4,27 @@ import argparse
 import os
 import format.wta as wta
 
-parser = argparse.ArgumentParser()
-parser.add_argument("wta_file", help=".wta file name")
-parser.add_argument("wtp_file", help=".wtp file name")
-parser.add_argument("directory", help="output directory")
 
-args = parser.parse_args()
-wta_file = open(args.wta_file, "rb")
-wtp_file = open(args.wtp_file, "rb")
+def unpack_wtp(wtp_filename, wta_filename, out_dir=""):
+    if not os.path.isfile(wtp_filename) or not os.path.isfile(wta_filename):
+        return  # skip if not exist
 
-wta = wta.File.parse(wta_file)
+    with open(wta_filename, "rb") as wta_file:
+        with open(wtp_filename, "rb") as wtp_file:
+            parsed_wta = wta.File.parse(wta_file)
+            for id, f in enumerate(parsed_wta.textures):
+                out_filename = wtp_filename + "_" + str(id).zfill(3) + ".dds"
+                with open(os.path.join(out_dir, out_filename), "wb") as out_file:
+                    wtp_file.seek(f.offset)
+                    content = wtp_file.read(f.size)
+                    out_file.write(content)
 
-for id, f in enumerate(wta.textures):
-    out_filename = os.path.basename(args.wta_file) + "_" + str(id).zfill(3) + ".dds"
-    with open(os.path.join(args.directory, out_filename), "wb") as out_file:
-        wtp_file.seek(f.offset)
-        content = wtp_file.read(f.size)
-        out_file.write(content)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("wta_file", help=".wta file name")
+    parser.add_argument("wtp_file", help=".wtp file name")
+    parser.add_argument("directory", help="output directory")
+
+    args = parser.parse_args()
+    unpack_wtp(args.wtp_file, args.wta_file, os.path.basename(args.directory))
