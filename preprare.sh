509242ae20f0ec1a66ee188609e168990b56487a
Syn Spawacza <synspawacza@gmail.com>
2020-05-21 13:55:58 +0200

    extracting cjk characters from ui_loading

--- preprare.sh
+++ preprare.sh
@@ -22,6 +22,15 @@ function extract_mcd() {
     ./tools/unpack_font.py --font-id ${font} --skip-cjk ${mcd} ${out_dir} ${texture}
 }
 
+function extract_mcd_with_cjk() {
+    mcd=$1
+    font=$2
+    echo "Extracting font ${font} from ${mcd}"
+    texture=`echo $mcd | sed -e 's/\.dat/.dtt/' -e 's/\.mcd/.wtp_000.dds/'`
+    out_dir=fonts/${font}
+    ./tools/unpack_font.py --font-id ${font} ${mcd} ${out_dir} ${texture}
+}
+
 for lang in "" "_us" "_fr" "_it" "_de" "_es"
 do
     extract_mcd unpacked/ui/ui_title${lang}.dat/messtitle.mcd 02
@@ -41,7 +50,9 @@ do
     extract_mcd unpacked/ui/ui_core_2${lang}.dat/messcore_2.mcd 09
     extract_mcd unpacked/ui/ui_dbg${lang}.dat/messdbg.mcd 10
 done
-extract_mcd unpacked/ui/ui_loading.dat/messloading.mcd 02
+extract_mcd_with_cjk unpacked/ui/ui_loading.dat/messloading.mcd 01
+extract_mcd_with_cjk unpacked/ui/ui_loading.dat/messloading.mcd 02
+extract_mcd_with_cjk unpacked/ui/ui_loading.dat/messloading.mcd 05
 ./tools/unpack_font.py --char $((16#2514)) --font-id 36 unpacked/ui/ui_chapter_us.dat/messchapter.mcd fonts/01 unpacked/ui/ui_chapter_us.dtt/messchapter.wtp_000.dds
 ./tools/unpack_font.py --char $((16#251c)) --font-id 36 unpacked/ui/ui_chapter_us.dat/messchapter.mcd fonts/01 unpacked/ui/ui_chapter_us.dtt/messchapter.wtp_000.dds
 
